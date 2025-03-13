a5e5c42753555ea6764648d00d8fd014f9813539
Jakub Neruda <nerudaj@email.cz>
2025-03-13 12:09:30 +0100

    Update SemanticTypes.hpp

--- lib/include/types/SemanticTypes.hpp
+++ lib/include/types/SemanticTypes.hpp
@@ -5,3 +5,23 @@
 
 using Direction = BrandedType<sf::Vector2f, struct DirectionTypeTag>;
 using Position = BrandedType<sf::Vector2f, struct PositionTypeTag>;
+
+consteval float operator""_px_per_second(unsigned long long value)
+{
+    return static_cast<float>(value);
+}
+
+consteval bool operator""_true(const char*, size_t)
+{
+    return true;
+}
+
+consteval bool operator""_false(const char*, size_t)
+{
+    return false;
+}
+
+/* Example usage:
+auto v = 10_px_per_second;
+auto f = "disabled"_true;
+*/
