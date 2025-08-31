6ddd1e66aa46f5187e33865831edd94f2d7a6dac
Jakub Neruda <nerudaj@email.cz>
2025-08-31 18:55:30 +0200

    Mainly docstring changes

--- lib/include/filesystem/ResourceLoader.hpp
+++ lib/include/filesystem/ResourceLoader.hpp
@@ -7,8 +7,21 @@
 class ResourceLoader final
 {
 public:
+    /**
+     *  \brief Loads all assets from the directory where the application is
+     * installed and stores them within a resource manager instance.
+     *
+     *  On Android, assets are packed inside the application itself and
+     * extracted through Android NDK interface (unlike AppStorage that uses
+     * regular C++ APIs).
+     */
     static dgm::ResourceManager
     loadResources(const std::filesystem::path& assetDir);
 
+    /**
+     *  \brief Load application settings from the AppStorage location
+     *
+     *  Any errors are converted into exceptions and thrown.
+     */
     static AppSettings loadSettings(const std::filesystem::path& file);
 };
