d512e4a5d68be657aabe2c93fbcc326a0eae6a8d
Jakub Neruda <nerudaj@email.cz>
2026-01-10 20:56:28 +0100

    Update Android NDK version
    Also use current cmake version for gradle.build.kts.in

--- cmake/vars.cmake
+++ cmake/vars.cmake
@@ -14,7 +14,7 @@ set ( INSTALLER_ABOUT_URL "https://mygame.org/" )
 set ( ANDROID_ORG "org.nerudaj.${PROJECT_NAME_LOWERCASE}" )
 set ( ANDROID_NATIVE_LIB_NAME "cppcore" )
 set ( ANDROID_ARCH_ABI "arm64-v8a" )
-set ( ANDROID_NDK_VERSION "26.1.10909125" )
+set ( ANDROID_NDK_VERSION "27.0.12077973" )
 set ( ANDROID_MIN_SDK "21" )
 set ( ANDROID_TARGET_SDK "33" )
 set ( ANDROID_ENFORCED_ORIENTATION "landscape" ) # landscape | portrait
